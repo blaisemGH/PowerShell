a178d966dd7835ea134e9e956b7aa8fd417144e1
blaisemGH <81759796+blaisemGH@users.noreply.github.com>
2023-03-02 09:29:05 +0100

    Update Find-StringRecursively.ps1
    Fixed bug due to typo

--- Find-StringRecursively.ps1
+++ Find-StringRecursively.ps1
@@ -1,37 +1,36 @@
 Function Find-StringRecursively {
-
     [CmdletBinding(DefaultParameterSetName='RegExPattern')]
     Param (
         [Parameter(
             Mandatory=$true,
             ValueFromPipelineByPropertyName=$true,
-            Position=1
-        )]
+            Position=1)]
+        [alias('Path')]
         [string]
             $FullName = '.',
-        [Parameter(Mandatory=$true, ParameterSetName = 'RegExPattern', Position=0)]
+        [Parameter(Mandatory=$true, ParameterSetName = 'RegExPattern' , Position=0)]
         [Parameter(Mandatory=$true, ParameterSetName = 'SimplePattern', Position=0)]
         [string]
             $Pattern,
-        [Parameter(Mandatory=$true, ParameterSetName = 'RegExNotMatch')]
+        [Parameter(Mandatory=$true, ParameterSetName = 'RegExNotMatch' )]
         [Parameter(Mandatory=$true, ParameterSetName = 'SimpleNotMatch')]
         [string]
             $NotMatch,
-        [Parameter(ParameterSetName = 'RegExPattern')]
-        [Parameter(ParameterSetName = 'RegExNotMatch')]
+        [Parameter(ParameterSetName = 'RegExPattern'  )]
+        [Parameter(ParameterSetName = 'RegExNotMatch' )]
         [switch]
             $AllMatches,
-        [Parameter(ParameterSetName = 'SimplePattern')]
+        [Parameter(ParameterSetName = 'SimplePattern' )]
         [Parameter(ParameterSetName = 'SimpleNotMatch')]
         [switch]
             $SimpleMatch,
-        [alias('f')]
         [string]
-            $FileFilter = '*',
+            $FilterFile = '*',
         [alias('nr')]
         [switch]
             $NoRecurse,
-        [alias('s')]
+        [switch]
+            $Force,
         [switch]
             $CaseSensitive,
         [int[]]
@@ -56,7 +55,7 @@ Function Find-StringRecursively {
     begin {
         $ansi = [char]27
         $odir = Convert-Path (PWD)
-        $Pattern  = $Pattern -Replace '^([*+}])$','.$1' #((?>^|[^.)}[\]]))([*+?])','$1.$2'
+        $Pattern  = $Pattern -Replace '^([*+}])$' , '.$1' #((?>^|[^.)}[\]]))([*+?])' , '$1.$2'
 
         $GCIparams = @{'File' = $true}
         $recurse = & {
@@ -67,46 +66,45 @@ Function Find-StringRecursively {
                 'TopDirectoryOnly'
             }
         }
-        If ( $Filter ) {
-            $GCIparams.Add( 'Filter' , $Filter)
+        If ( $FilterFile ) {
+            $GCIparams.Add( 'Filter' , $FilterFile)
         }
 
         $contextPointer = $null
         $SLSparams = @{}
-        $SLSparams.Add(    'LiteralPath'    , $Null)
+        $SLSparams.Add(     'LiteralPath'   , $Null)
         If ( $Pattern ) {
-            $SLSparams.Add( 'Pattern'    , $Pattern)
+            $SLSparams.Add( 'Pattern'       , $Pattern)
         }
         If ( $NotMatch ) {
-            $SLSparams.Add( 'NotMatch'    , $NotMatch)
+            $SLSparams.Add( 'NotMatch'      , $NotMatch)
         }
         If ( $AllMatches ) {
             $SLSparams.Add( 'AllMatches'    , $true)
         }
         If ( $SimpleMatch ) {
-            $SLSparams.Add( 'SimpleMatch'    , $SimpleMatch)
+            $SLSparams.Add( 'SimpleMatch'   , $SimpleMatch)
         }
-            If ( $CaseSensitive ) {
-                $SLSparams.Add( 'CaseSensitive' , $CaseSensitive)
-            }
+        If ( $CaseSensitive ) {
+            $SLSparams.Add( 'CaseSensitive' , $CaseSensitive)
+        }
         If ( $Context ) {
-            $SLSparams.Add( 'Context' , $Context)
-            #$contextPointer    = "$ansi[38;5;202m>:$ansi[0m"
-            $preContextSkip        = [Environment]::NewLine * $Context[0]
+            $SLSparams.Add( 'Context'   , $Context)
+			#$contextPointer = "$ansi[38;5;202m>:$ansi[0m"
         }
         If ( $Encoding ) {
-            $SLSparams.Add( 'Encoding'    , $Encoding)
+            $SLSparams.Add( 'Encoding'  , $Encoding)
         }
         If ( $List ) {
-            $SLSparams.Add( 'List'    , $true)
+            $SLSparams.Add( 'List'  , $true)
         }
         If ( $Quiet ) {
-            $SLSparams.Add( 'Quiet'    , $true)
+            $SLSparams.Add( 'Quiet' , $true)
         }
     }
     process {
         $fileList = [Collections.Generic.List[string]]@()
-        $path = If ( $fullName -in '*','.' ) {
+        $path = If ( $fullName -in '*' , '.' ) {
             $odir
         }
         Else {
@@ -115,10 +113,10 @@ Function Find-StringRecursively {
         Foreach ( $fullPath in (Convert-Path $path) ) {
             $cleanPath = $fullPath
             try {
-                [string[]]$subFolderfileList = [System.IO.Directory]::GetFiles( $cleanPath, $Filter, $recurse )
+                [string[]]$subFolderfileList = [System.IO.Directory]::GetFiles( $cleanPath, $FilterFile, $recurse )
             }
             catch {
-                [string[]]$subFolderfileList = (Get-ChildItem $cleanPath @GCIParams -Recurse:(!$NoRecurse)).FullName
+                [string[]]$subFolderfileList = ( Get-ChildItem $cleanPath @GCIParams -Recurse:(!$NoRecurse) -Force:$Force ).FullName
             }
             $fileList.AddRange($subFolderfileList)
         }
@@ -127,15 +125,15 @@ Function Find-StringRecursively {
         Foreach ( $file in $fileList ) {
             $SLSParams.LiteralPath = $file
             Select-String @SLSparams | ForEach-Object {
-                $filePath = $_.path.replace($odir,'.')
+                $filePath = $_.path.replace($odir , '.')
                 if ( $Context) {
-                    $preCount = @($_.Context.PreContext.split([Environment]::NewLine)).GetUpperBound(0) + 1
+                    $preCount = @($_.Context.PreContext.split([Environment]::NewLine)).Count
                     $preLineNumber = ''
-                    $postCount = @($_.Context.PostContext.split([Environment]::NewLine)).GetUpperBound(0) + 1
+                    $postCount = @($_.Context.PostContext.split([Environment]::NewLine)).Count
                     $postLineNumber = ''
                     If ( $preCount ) {
-                        ForEach ($LineNumber in $preCount..1) {
-                            $preLineNumber += ($_.LineNumber - $LineNumber).ToString() + [Environment]::NewLine
+                        ForEach ($LineNumber in $preCount..1 ) {
+                            $preLineNumber  += ($_.LineNumber - $LineNumber).ToString() + [Environment]::NewLine
                         }
                     }
                     If ( $postCount ) {
@@ -144,12 +142,11 @@ Function Find-StringRecursively {
                         }
                     }
                 }
-                
-                #Contents replaces "`a", because this is present in some binary files (I found in a .class java file) and is the alert special character (beep sound)
+
                 [PSCustomObject]@{
-                    Filename    = "{0,-$maxFileLength}" -f ("$ansi[38;5;202m"    +    ($filePath.substring([math]::max($filePath.Length - 80, 0)))     +    ":$ansi[0m")
-                    Line        = '{0,-9}' -f ( $preLineNumber + "$ansi[93m" + $_.LineNumber    +    ">$ansi[0m" + $postLineNumber )
-                    Contents    = '{0}{1}{2}' -f ($_.Context.PreContext | Out-String), ($contextPointer + $_.Line), ($_.Context.PostContext | Out-String) -replace "$([Environment]::NewLine)$" -Replace "`a"
+                    Filename  = "{0 , -$maxFileLength}" -f ("$ansi[38;5;202m"  +  ($filePath.substring([math]::max($filePath.Length - 80, 0)))   +  ":$ansi[0m")
+                    Line      = '{0 , -9}' -f ( $preLineNumber + "$ansi[93m" + $_.LineNumber  +  ">$ansi[0m" + $postLineNumber )
+                    Contents  = '{0}{1}{2}' -f ($_.Context.PreContext | Out-String), ($contextPointer + $_.Line), ($_.Context.PostContext | Out-String) -replace "$([Environment]::NewLine)$" #'{0, -155}' -f $_.Line
                 } | Format-Table -HideTableHeaders -Wrap
             }
         }
